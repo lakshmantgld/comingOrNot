207f02cbda0732c7ecab1d0ef0a0400b5b0390f6
lakshman <diwaakartg@gmail.com>
2016-07-15 14:03:28 +0530

    apis to store an event when it is created in index page

--- routes/api/registerEvent.js
+++ routes/api/registerEvent.js
@@ -4,26 +4,15 @@ let router = express.Router();
 import event from './../../models/event';
 
 router.post('/', (req, res) => {
-  console.log('coming inside the request');
   let name = req.body.name;
   let purpose = req.body.purpose;
   let dateArray = req.body.dateArray;
-
-  console.log('name ' + name + '   purpose ' + purpose + '  dateArray  = ' + dateArray);
-
-  console.log('now printing the event model itself');
-  console.log(event);
-  console.log(JSON.stringify(event));
 
   event.save(name, purpose, dateArray, (error, eventObj) => {
     if (error) {
       console.log(error);
       return res.status(500).send(error);
     }
-
-    console.log('successfully saved');
-    console.log('and the id is ');
-    console.log(eventObj._id);
 
     res.json({'eventId': eventObj._id});
   });
