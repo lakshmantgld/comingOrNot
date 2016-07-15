c028d3f234326144377bc3aec82f6e71373cffd1
lakshman <diwaakartg@gmail.com>
2016-07-15 14:02:56 +0530

    Apis for fething an event to display it in event table in eventpage

--- routes/api/fetchEvent.js
+++ routes/api/fetchEvent.js
@@ -4,15 +4,12 @@ let router = express.Router();
 import event from './../../models/event';
 
 router.get('/', (req, res) => {
-  console.log('coming inside the fetchEvent');
   const eventId = req.query.eventId;
 
   event.getEvent(eventId, (err, eventObj) => {
     if (err) {
       return res.status(500).send(err);
     }
-    console.log('printn the event obj');
-    console.log(eventObj);
     res.json(eventObj);
   });
 });
