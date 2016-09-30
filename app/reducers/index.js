fbc90066395a9bc26dcfd853710210dbf840d142
Lakshman Diwaakar <lakshman@Lakshmans-MacBook-Pro.local>
2016-09-30 17:49:26 +0900

    index.js merge conflict solved

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,13 +3,9 @@ import { routerReducer } from 'react-router-redux';
 
 import { name, purpose, latlngs, sourceLabel, destinationLabel, dateArray, dateArrayErrorLabel,
         nameErrorLabel, purposeErrorLabel, eventObj, personalizedDateSelection, attendeeName,
-<<<<<<< HEAD
-        attendeeNameErrorLabel, toggleCastAttendance, attendeeNameEmptyFlag, attendeeNameExistsFlag, registerSuccessFlag, updateSuccessFlag,location, updateAttendeeDate, updateAttendeeId,
-        updateAttendeeName, languageJson } from './registerReducers';
-=======
-        attendeeNameErrorLabel, toggleCastAttendance, location, updateAttendeeDate, updateAttendeeId,
+        attendeeNameErrorLabel, toggleCastAttendance, attendeeNameEmptyFlag, attendeeNameExistsFlag,
+        registerSuccessFlag, updateSuccessFlag,location, updateAttendeeDate, updateAttendeeId,
         updateAttendeeName, languageJson, weather } from './registerReducers';
->>>>>>> weather
 
 const reducers = combineReducers({
   name,
