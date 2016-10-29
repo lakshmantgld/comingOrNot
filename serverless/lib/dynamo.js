c9d21a5796604e53c07e201221c58fb9c4d09c65
lakshman <diwaakartg@gmail.com>
2016-10-29 10:22:44 +0900

    introduced graphQL and deployed services in aws using serverless #53 #58

---
+++ serverless/lib/dynamo.js
@@ -0,0 +1,87 @@
+import Promise from 'bluebird';
+import AWS from 'aws-sdk';
+import moment from 'moment';
+
+AWS.config.update({
+  accessKeyId: 'AKIAIJ63MR2PIAW4LSPQ',
+  secretAccessKey: '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',
+  region: 'ap-northeast-1'
+});
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+// gives a single event from eventId
+export function fetchEvent(eventId) {
+
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: "event",
+      Key: {
+        "eventId": eventId
+      }
+    };
+
+    dynamo.get(params, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(data.Item);
+    });
+  });
+}
+
+// creates a new event and sends back the created event with eventID
+export function createEvent(event) {
+  console.log("in dynamo");
+  event.eventId = moment().format('YYYYMMDDhmms') + '-' + event.name + '-' + event.purpose;
+
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: "event",
+      Item: event
+    };
+
+    dynamo.put(params, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(event);
+    });
+  });
+}
+
+// adds a new attendee to the existing event based on eventId and returns back the whole event.
+export function registerAttendee(eventAttendee) {
+  console.log("comim in registerAttendee");
+  console.log(eventAttendee);
+  return new Promise((resolve, reject) => {
+
+    const attendee = {
+      "attendeeName": eventAttendee.attendeeName,
+      "personalizedDateSelection": eventAttendee.personalizedDateSelection
+    }
+    const attendeeList = [attendee];
+    const eventId = eventAttendee.eventId;
+
+    const params = {
+      TableName: "event",
+      Key: {
+        "eventId": eventId
+      },
+      UpdateExpression: "SET attendees = list_append(attendees, :attendee)",
+      ExpressionAttributeValues: {
+        ":attendee" : attendeeList
+      },
+      ReturnValues: "ALL_NEW"
+    };
+
+    dynamo.update(params, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      console.log("in update dynamo");
+      console.log(data.Attributes);
+      return resolve(data.Attributes);
+    });
+  });
+}
