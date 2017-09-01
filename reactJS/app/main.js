846e54aaef375d95d30c7a769cba7001c41df670
lakshman <lakshman@fx24106.local>
2017-09-01 21:16:36 +0900

    react-GA code added

--- reactJS/app/main.js
+++ reactJS/app/main.js
@@ -8,13 +8,19 @@ import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import {syncHistoryWithStore} from 'react-router-redux';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
+import ReactGA from 'react-ga';
 
+import config from '../config/config.json';
 import reducers from './reducers';
-
 import App from './components/App';
 import RegisterComponent from './components/RegisterComponent';
 import EventShareComponent from './components/EventShareComponent';
 import EventPageComponent from './components/EventPageComponent';
+
+ReactGA.initialize(config.googleAnalytics, {
+  debug: true,
+  titleCase: false,
+});
 
 window.React = React;
 
@@ -33,13 +39,26 @@ browserHistory.listen(location => {
    }
  });
 
+const trackIndexPage = () => {
+  console.log("trackIndexPage");
+  console.log(config.googleAnalytics);
+  ReactGA.set({ page: '/' });
+  ReactGA.pageview('/');
+}
+
+const trackEventPage = () => {
+  console.log("trackEventPage");
+  ReactGA.set({ page: '/event' });
+  ReactGA.pageview('/event');
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
       <Route path='/' component={App}>
-        <IndexRoute component={RegisterComponent} />
+        <IndexRoute onEnter={trackIndexPage} component={RegisterComponent} />
         <Route path='event/' component={EventPageComponent}>
-          <Route path=':eventId' component={EventPageComponent}>
+          <Route path=':eventId' onEnter={trackEventPage} component={EventPageComponent}>
           </Route>
         </Route>
       </Route>
