2f123fa545049ce2c5e17980815e3234a2ed2b02
Suresh Murali <m.suresh.fx@gmail.com>
2017-11-19 17:10:17 +0900

    Footer layout fix

--- reactJS/app/components/Footer.js
+++ reactJS/app/components/Footer.js
@@ -21,10 +21,10 @@ class BottomNavigationExampleSimple extends Component {
   render() {
     return (
       <div className='row footer-row'>
-        <div className='col-md-5 col-sm-5 col-xs-6 ft-left'>
+        <div className='col-sm-offset-1 col-md-5 col-sm-5 col-xs-6'>
           <a href='/privacypolicy' style={{'color': 'grey'}}><span>Privacy Policy</span></a>
         </div>
-        <div className='col-md-5 col-sm-5 col-xs-6 ft-right'>
+        <div className='col-md-5 col-sm-5 col-xs-6 end-xs'>
           <span>Copyright ©2017 Comingornot</span>
         </div>
       </div>
